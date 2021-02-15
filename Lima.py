d5c9ebdbeb240beb6bb19143a1ebc57c6433e795
Rodrigo <green.tie.rgm@gmail.com>
2021-02-14 21:33:42 -0600

    Position + Borderless

--- Lima.py
+++ Lima.py
@@ -1,10 +1,24 @@
 import time, tkinter as tk
 
+
+def position_window(root, width, height):
+    x = root.winfo_screenwidth() - width
+    y = root.winfo_screenheight() - height
+    root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+
+
 # Properties
 prop_stayontop =    True
 prop_borderless =   True
+prop_anchor =       "NE"
 
 root = tk.Tk()
+
+width =  250
+height = 100
+position_window(root, width, height)
+
 
 but = tk.Button(root, text="Lol")
 but.pack()
@@ -12,4 +26,5 @@ but.pack()
 if (prop_stayontop):
     root.call('wm', 'attributes', '.', '-topmost', '1')
 
+root.overrideredirect(True)
 root.mainloop()
